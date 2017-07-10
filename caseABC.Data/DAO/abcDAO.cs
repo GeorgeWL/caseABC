4dc7f3baba4ec908d58165b051e2be627d61598c
GeorgeWL <georgelangham1990@gmail.com>
2017-07-10 22:54:48 +0100

    DAO

---
+++ caseABC.Data/DAO/abcDAO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using caseABC.Data;
+using caseABC.Data.DAO;
+using caseABC.Data.IDAO;
+
+namespace caseABC.Data.DAO
+{
+    //DAO class inherits from the IDAO Class
+    public class abcDAO : IabcDAO
+    {
+        //stores ABCentities locally into a db context
+        //private caseABCEntities _context;
+        //use local context to create new objects
+        //public abcDAO()
+        //{
+        //    _context = new caseABCEntities();
+        //}
+        public IList<Parcel> GetCustomerParcels(int CustomerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateCustomerParcel(int ParcelId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditCustomerParcel(int ParcelId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCustomerParcel(int ParcelId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Tracking> GetTrackingEvents(int TrackingId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
